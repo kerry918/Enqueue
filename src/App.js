aa5a5db677fbcf5bc2db0d3d263b1e8acf34ecfc
jettbui <jettcbui@gmail.com>
2021-02-19 21:09:50 -0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import LandingPage from './Components/LandingPage/LandingPage';
+import SitePage from "./Components/SitePage/SitePage";
 
 function App() {
   return (
     <div>
-      <LandingPage/>
+      <Router>
+        <Switch>
+          <Route exact path="/site" component={SitePage} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
