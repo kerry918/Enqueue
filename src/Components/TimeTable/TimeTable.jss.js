5a2ae3e9ad07b6ffde52146d420bb6b40e9d00df
jettbui <jettcbui@gmail.com>
2021-02-20 13:48:55 -0800

    placeholder TimeTable

---
+++ src/Components/TimeTable/TimeTable.jss.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2%', 
+        paddingTop: '1%'
+    }
+}));
+
+export default useStyles;
