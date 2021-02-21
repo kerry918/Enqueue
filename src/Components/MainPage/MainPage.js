f7d68c51452f08e30726eb3016e7b025f4d5b64b
Kerry Liu <kerryliukerui@gmail.com>
2021-02-20 22:08:41 -0500

    Added few img

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -3,7 +3,10 @@ import SiteCard from "../SiteCard/SiteCard";
 
 import { Button } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import useStyles from './MainPage.jss'
+import useStyles from './MainPage.jss'; 
+ 
+import vaccineImg from '../../Assets/Images/vaccine.jpg'; 
+import virusImg from '../../Assets/Images/virus.jpg'; 
 
 export default function LandingPage() {
     const classes = useStyles(); 
@@ -13,6 +16,8 @@ export default function LandingPage() {
             <Button href="/" startIcon={<ArrowBackIcon />}>Back to Main</Button>
             <h1>Main Page Placeholder</h1>
             <SiteCard/>
+            <img src={vaccineImg} className={classes.img1}/>
+            <img src={virusImg} className={classes.img2}/>
         </div>
     )
 }
--- src/Components/MainPage/MainPage.jss.js
+++ src/Components/MainPage/MainPage.jss.js
@@ -3,7 +3,19 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: '0 5%'
-    }
+    }, 
+    img1: {
+        position: 'absolute', 
+        width: '40%', 
+        right: 0, 
+        bottom: 0
+    }, 
+    img2: {
+        position: 'absolute', 
+        width: '30%', 
+        right: 100, 
+        top: 30
+    }, 
 }));
 
 export default useStyles;
