c35cad5835d713076fd932807ff9a136b3a30921
brianjhuang <brianjhuang25@gmail.com>
2021-02-21 09:03:25 -0800

    reverted

--- myflaskapp/app.py
+++ myflaskapp/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Articles
+#from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, BooleanField, IntegerField
-#from passlib.hash import sha256_crypt
+from passlib.hash import sha256_crypt
 from functools import wraps
 
 app = Flask(__name__)
@@ -33,7 +33,11 @@ class RegisterForm(Form):
     email = StringField('Email', [validators.Length(min=6, max=100)])
     age = IntegerField('Age', [validators.Required()])
     immune_status = BooleanField('Immunocompromised', [validators.Required()])
-    
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
@@ -44,12 +48,13 @@ def register():
         username = form.username.data
         age = form.age.data
         immune_status = form.immune_status.data
+        password = sha256_crypt.encrypt(str(form.password.data))
 
         # Create cursor
         cur = mysql.connection.cursor()
 
         # Execute query
-        cur.execute("INSERT INTO users(name, email, age, immune_status) VALUES(%s, %s, %s, %s)", (name, username, age, immune_status))
+        cur.execute("INSERT INTO users(name, email, age, immune_status, password) VALUES(%s, %s, %s, %s, %s)", (name, username, age, immune_status, password))
 
         # Commit to DB
         mysql.connection.commit()
